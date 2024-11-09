6eaec6f65b99c351d2e0d61a74c7aef764c25456
Gabriel Ángel Canals Salleras <alu0101460468@ull.edu.es>
2024-11-09 10:47:56 +0000

    Crear ficheros backend

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+// simple express server
+const express = require('express');
+
+export const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
