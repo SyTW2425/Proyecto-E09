0aedf4fd879d1e91c17b669f4c69bfac74394283
Gabriel Ángel Canals Salleras <alu0101460468@ull.edu.es>
2024-11-11 16:01:24 +0000

    Some changes for login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'login', component: LoginFormComponent },
+	//{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+];
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,87 @@
+import { Component, effect, EventEmitter, input, Output } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import {User} from '../user';
+
+@Component({
+	selector: 'app-login',
+	standalone: true,
+	imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+  ],
+	template: `
+
+<form class="login-form" autocomplete="off" [formGroup]="loginForm" (submit)="submitForm()">
+  <mat-form-field>
+    <mat-label>Usuario</mat-label>
+    <input matInput formControlName="username" placeholder="Usuario" required>
+    <small *ngIf="username.invalid && (username.dirty || username.touched)">
+      El usuario es requerido
+    </small>
+  </mat-form-field>
+  
+  <mat-form-field>
+    <mat-label>Contraseña</mat-label>
+    <input matInput formControlName="password" placeholder="Contraseña" required>
+    <small *ngIf="password.invalid && (password.dirty || password.touched)">
+      La contraseña es requerida
+    </small>
+  </mat-form-field>
+  
+  <button type="submit" class="btn btn-primary" [disabled]="loginForm.invalid">Iniciar sesión</button>
+</form>
+
+	`,
+	styles: [`
+	.employee-form {
+		width: 50%;
+		margin: 0 auto;
+	}
+	`]
+})
+export class LoginFormComponent {
+	initialState = input<User>();
+
+	@Output()
+	formValuesChanged = new EventEmitter<User>();
+
+	@Output()
+	formSubmitted = new EventEmitter<User>();
+
+	constructor(private formBuilder: FormBuilder) {
+		effect(() => {
+			this.loginForm.setValue({
+				username: this.initialState()?.username || '',
+				password: this.initialState()?.password || '',
+			});
+		});
+	}
+
+	loginForm = this.formBuilder.group({
+		username: ['', Validators.required],
+		password: ['', Validators.required]
+	});
+
+	get username() {
+    return this.loginForm.get('username')!;
+  }
+
+	get password() {
+		return this.loginForm.get('password')!;
+	}
+
+	get email() {
+		return this.loginForm.get('email')!;
+	}
+
+  submitForm() {
+    this.formSubmitted.emit(this.loginForm.value as User);
+  }
+}
