6d1ceea58639385d3e83752d231776fae9ada8db
Gabriel Ángel Canals Salleras <alu0101460468@ull.edu.es>
2025-01-04 17:21:09 +0000

    Update env in front

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/auth' // URL para producción
+  apiUrl: 'http://localhost:3000' // URL para producción
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/auth' // URL para desarrollo
+  apiUrl: 'http://localhost:3000' // URL para desarrollo
 };
